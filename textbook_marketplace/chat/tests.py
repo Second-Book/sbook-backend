daf15dee8211b963e47f3612f6b46c77cf9f9f59
Sasha <Geimer0073@yandex.ru>
2025-11-28 21:24:50 +0500

    add tests for new ws functionality

--- textbook_marketplace/chat/tests.py
+++ textbook_marketplace/chat/tests.py
@@ -46,22 +46,27 @@ async def test_call_group_send_twice_success(channel_layer):
 
 
 @pytest.fixture
+def ws_url() -> str:
+    return 'ws/chat/'
+
+
+@pytest.fixture
 def first_user() -> User:
-    yield User.objects.create(
+    yield User.objects.create_user(
         username='testusername1', password='testpassword1'
     )
 
 
 @pytest.fixture
 def second_user() -> User:
-    yield User.objects.create(
+    yield User.objects.create_user(
         username='testusername2', password='testpassword2'
     )
 
 
 @pytest.fixture
 def third_user() -> User:
-    yield User.objects.create(
+    yield User.objects.create_user(
         username='testusername3', password='testpassword3'
     )
 
@@ -72,9 +77,9 @@ def client() -> APIClient:
 
 
 @pytest.fixture
-def test_messages(first_user: User,
-                  second_user: User,
-                  third_user: User) -> tuple:
+def test_unseen_messages(first_user: User,
+                         second_user: User,
+                         third_user: User) -> tuple:
     msg1 = Message.objects.create(text="hello user2, i'm user1",
                                   sender=first_user,
                                   recipient=second_user)
@@ -87,6 +92,25 @@ def test_messages(first_user: User,
     yield msg1, msg2, msg3
 
 
+@pytest.fixture
+def test_seen_messages(first_user: User,
+                       second_user: User,
+                       third_user: User) -> tuple:
+    msg1 = Message.objects.create(text="i'm user1, you saw me",
+                                  sender=first_user,
+                                  recipient=second_user,
+                                  seen=True)
+    msg2 = Message.objects.create(text='hello, user1, you saw me too',
+                                  sender=second_user,
+                                  recipient=first_user,
+                                  seen=True)
+    msg3 = Message.objects.create(text='hi, (seen)',
+                                  sender=third_user,
+                                  recipient=first_user,
+                                  seen=True)
+    yield msg1, msg2, msg3
+
+
 @pytest.fixture(scope='module')
 def application() -> JWTAuthMiddlewareStack:
     yield JWTAuthMiddlewareStack(
@@ -96,233 +120,221 @@ def application() -> JWTAuthMiddlewareStack:
     )
 
 
+@pytest.mark.django_db(reset_sequences=True)
 @pytest.mark.asyncio
-async def test_websocket_with_token(transactional_db,
-                                    application: JWTAuthMiddlewareStack,
-                                    first_user: User,
-                                    second_user: User):
-    chat_url = 'chat/new_test_room/'
+async def test_ws_conn_success(
+        ws_url: str,
+        first_user: User,
+        application: JWTAuthMiddlewareStack
+):
     token = AccessToken.for_user(first_user)
     communicator = WebsocketCommunicator(
-        application, f'{chat_url}?token={token}'
+        application, f'{ws_url}?token={token}'
     )
-    connected, subprotocol = await communicator.connect()
+    connected, subprotocol = await communicator.connect(timeout=1)
     assert connected
 
-    await communicator.send_json_to(
-        data={'message': 'test',
-              'sender': first_user.username,
-              'recipient': second_user.username}
+
+@pytest.mark.django_db(reset_sequences=True)
+@pytest.mark.asyncio
+async def test_retrieve_new_messages_after_ws_conn_success(
+        ws_url: str,
+        first_user: User,
+        test_unseen_messages: tuple[Message],
+        application: JWTAuthMiddlewareStack
+):
+    token = AccessToken.for_user(first_user)
+    communicator = WebsocketCommunicator(
+        application, f'{ws_url}?token={token}'
     )
-    response: Response = await communicator.receive_json_from()
-    assert response['sender'] == 'testusername1'
-    assert response['recipient'] == 'testusername2'
-    assert response['message'] == 'test'
+    connected, subprotocol = await communicator.connect(timeout=1)
+    assert connected
+    initial_message = await communicator.receive_json_from(timeout=1)
+    assert initial_message['type'] == 'notification'
+    assert len(initial_message['new_messages']) == 2
+    for msg in initial_message['new_messages']:
+        assert msg['recipient'] == 'testusername1'
+
+
+@pytest.mark.django_db(reset_sequences=True)
+@pytest.mark.asyncio
+async def test_seen_messages_not_retrieved_after_ws_conn(
+        ws_url: str,
+        first_user: User,
+        test_seen_messages: tuple[Message],
+        application: JWTAuthMiddlewareStack,
+):
+    token = AccessToken.for_user(first_user)
+    communicator = WebsocketCommunicator(
+        application, f'{ws_url}?token={token}'
+    )
+    connected, subprotocol = await communicator.connect(timeout=1)
+    assert connected
+    initial_message = await communicator.receive_json_from(timeout=1)
+    assert initial_message['type'] == 'notification'
+    assert len(initial_message['new_messages']) == 0
 
     await communicator.disconnect()
 
 
+@pytest.mark.django_db(reset_sequences=True)
 @pytest.mark.asyncio
-async def test_websocket_message_receive_success(
-        transactional_db,
+async def test_websocket_with_token_get_unseen_messages_success(
+        ws_url: str,
         application: JWTAuthMiddlewareStack,
         first_user: User,
-        second_user: User):
-    chat_url = 'chat/myroom/'
-    sender_token = AccessToken.for_user(first_user)
-    sender_communicator = WebsocketCommunicator(
-        application, f'{chat_url}?token={sender_token}'
+        second_user: User
+):
+    """
+    Testing that after ws connection user gets a 'notification' type message,
+    consisting of new (unseen) messages;
+    """
+    token_1 = AccessToken.for_user(first_user)
+    communicator_1 = WebsocketCommunicator(
+        application, f'{ws_url}?token={token_1}'
     )
-    connected, subprotocol = await sender_communicator.connect()
+    connected, subprotocol = await communicator_1.connect()
     assert connected
-
-    recipient_token = AccessToken.for_user(second_user)
-    recipient_communicator = WebsocketCommunicator(
-        application, f'{chat_url}?token={recipient_token}'
-    )
-
-    connected, subprotocol = await recipient_communicator.connect()
-    assert connected
-
-    await sender_communicator.send_json_to(
-        data={'message': 'test_message1',
+    response_1_init: Response = await communicator_1.receive_json_from()
+    assert response_1_init['type'] == 'notification'
+    await communicator_1.send_json_to(
+        data={'message': 'test123123NEW',
               'sender': first_user.username,
               'recipient': second_user.username}
     )
+    response_1_after_send: Response = await communicator_1.receive_json_from()
+    assert response_1_after_send['type'] == 'message'
+    assert response_1_after_send['sender'] == first_user.username
+    assert response_1_after_send['recipient'] == second_user.username
+    assert response_1_after_send['message'] == 'test123123NEW'
 
-    response = await recipient_communicator.receive_json_from()
-    assert response['message'] == 'test_message1'
-    assert response['sender'] == 'testusername1'
-    assert response['recipient'] == 'testusername2'
-
-    await sender_communicator.disconnect()
-    await recipient_communicator.disconnect()
-
-
-@pytest.mark.asyncio
-async def test_websocket_message_creation_success(
-        transactional_db,
-        application: JWTAuthMiddlewareStack,
-        first_user: User,
-        second_user: User):
-    chat_url = 'chat/test_room/'
-    token = AccessToken.for_user(first_user)
-    communicator = WebsocketCommunicator(
-        application, f'{chat_url}?token={token}'
+    token_2 = AccessToken.for_user(second_user)
+    communicator_2 = WebsocketCommunicator(
+        application, f'{ws_url}?token={token_2}'
     )
-    connected, subprotocol = await communicator.connect()
+    connected, subprotocol = await communicator_2.connect()
     assert connected
+    response_2_init: Response = await communicator_2.receive_json_from()
+    assert response_2_init['type'] == 'notification'
+    assert len(response_2_init['new_messages']) == 1
+    assert response_2_init['new_messages'][0]['text'] == 'test123123NEW'
 
-    await communicator.send_json_to(
-        data={'message': 'test123',
-              'sender': first_user.username,
-              'recipient': second_user.username}
-    )
-    first_response = await communicator.receive_json_from()
-    assert first_response['message'] == 'test123'
-
-    await communicator.send_json_to(
-        data={'message': 'test321',
-              'sender': second_user.username,
-              'recipient': first_user.username}
-    )
-    second_response = await communicator.receive_json_from()
-    assert second_response['message'] == 'test321'
-
-    messages = await sync_to_async(Message.objects.all)()
-    assert await sync_to_async(len)(messages) == 2
-    assert messages[0]
-
-    await communicator.disconnect()
-
-
-@pytest.mark.asyncio
-async def test_websocket_with_invalid_url(application: JWTAuthMiddlewareStack):
-    with pytest.raises(ValueError):
-        communicator = WebsocketCommunicator(
-            application, 'juifgjsiufj'
-        )
-        await communicator.connect()
-
-
-@pytest.mark.django_db(reset_sequences=True, transaction=True)
-def test_user_messages_retrieve_success(
-        first_user: User,
-        second_user: User,
-        third_user: User,
-        test_messages,
-        client: APIClient):
-    token = AccessToken.for_user(first_user)
-    client.force_authenticate(user=first_user, token=token)
-    response: Response = client.get(reverse('chat'))
-    assert len(response.data) == 3
-
-
-@pytest.mark.django_db(reset_sequences=True, transaction=True)
-def test_user_only_related_messages_retrieve_success(
-        first_user: User,
-        second_user: User,
-        test_messages,
-        client: APIClient):
-    token: AccessToken = AccessToken.for_user(second_user)
-    client.force_authenticate(user=second_user, token=token)
-    response: Response = client.get(reverse('chat'))
-    data = response.data
-    assert len(response.data) == 2
-    assert data[0]['sender'] == 1
-    assert data[0]['recipient'] == 2
-    assert data[0]['text'] == "hello user2, i'm user1"
-
-
-@pytest.fixture
-def blocking_user() -> User:
-    yield User.objects.create(username='blocker_user', password='password')
-
-
-@pytest.fixture
-def blocked_user() -> User:
-    yield User.objects.create(username='blocked_user', password='password')
-
-
-@pytest.fixture
-def block(blocking_user: User, blocked_user: User) -> Block:
-    yield Block.objects.create(initiator_user=blocking_user,
-                               blocked_user=blocked_user)
-
-
-@pytest.mark.django_db(reset_sequences=True, transaction=True)
-@pytest.mark.asyncio
-async def test_blocked_user_cannot_message(application: JWTAuthMiddlewareStack,
-                                           blocking_user: User,
-                                           blocked_user: User, block: Block):
-    room_url = 'chat/brand_new_test_room/'
-    token = AccessToken.for_user(blocked_user)
-    communicator = WebsocketCommunicator(
-        application, f'{room_url}?token={token}'
-    )
-    connected, subprotocol = await communicator.connect()
-    assert connected
-
-    await communicator.send_json_to(
-        data={'message': 'test',
-              'sender': blocked_user.username,
-              'recipient': blocking_user.username}
-    )
-
-    response: Response = await communicator.receive_json_from()
-    assert response['message'] == 'You cannot message this user due to block.'
-    # assert that no message objects has been created
-    assert await sync_to_async(Message.objects.all().exists)() is False
-
-    await communicator.disconnect()
-
-
-@pytest.mark.django_db(reset_sequences=True, transaction=True)
-@pytest.mark.asyncio
-async def test_websocket_message_blocked_success(
-        application: JWTAuthMiddlewareStack,
-        blocking_user: User,
-        blocked_user: User,
-        block: Block) -> None:
-    room_url = 'chat/blocked_room/'
-    sender_token = AccessToken.for_user(blocking_user)
-    sender_communicator = WebsocketCommunicator(
-        application, f'{room_url}?token={sender_token}'
-    )
-    connected, subprotocol = await sender_communicator.connect()
-    assert connected
-
-    recipient_token = AccessToken.for_user(blocked_user)
-    recipient_communicator = WebsocketCommunicator(
-        application, f'{room_url}?token={recipient_token}'
-    )
-
-    connected, subprotocol = await recipient_communicator.connect()
-    assert connected
-
-    await sender_communicator.send_json_to(
-        data={'message': 'test_message1',
-              'sender': blocked_user.username,
-              'recipient': blocking_user.username}
-    )
-    # Checking that blocked user couldn't send message to the recipient
+    # unseen messages wont be copied to 'message' type messages
     with pytest.raises(TimeoutError):
-        await recipient_communicator.receive_json_from()
+        await communicator_2.receive_json_from()
 
-    await sender_communicator.disconnect()
-    await recipient_communicator.disconnect()
+    await communicator_1.disconnect()
+    await communicator_2.disconnect()
 
 
-@pytest.mark.django_db(reset_sequences=True, transaction=True)
+@pytest.mark.django_db(reset_sequences=True)
 @pytest.mark.asyncio
-async def test_websocket_connect_decline(
+async def test_websocket_with_token_get_new_message_success(
+        ws_url: str,
         application: JWTAuthMiddlewareStack,
-        blocking_user: User):
-    room_url = 'chat/brand_new_test_room/'
-    blocked_user = AnonymousUser()
-    token = AccessToken.for_user(blocked_user)
-    communicator = WebsocketCommunicator(
-        application, f'{room_url}?token={token}'
+        first_user: User,
+        second_user: User
+):
+    token_1 = AccessToken.for_user(first_user)
+    communicator_1 = WebsocketCommunicator(
+        application, f'{ws_url}?token={token_1}'
     )
-    connected, subprotocol = await communicator.connect()
-    assert connected is False
+    connected, subprotocol = await communicator_1.connect()
+    assert connected
+
+    token_2 = AccessToken.for_user(second_user)
+    communicator_2 = WebsocketCommunicator(
+        application, f'{ws_url}?token={token_2}'
+    )
+    connected, subprotocol = await communicator_2.connect()
+    assert connected
+
+    await communicator_1.send_json_to(
+        data={'message': 'hi, user2',
+              'sender': first_user.username,
+              'recipient': second_user.username})
+
+    response_1_init = await communicator_1.receive_json_from(timeout=5)
+    assert response_1_init['type'] == 'notification'
+    assert len(response_1_init['new_messages']) == 0
+    response_1 = await communicator_1.receive_json_from(timeout=5)
+    assert response_1['message'] == 'hi, user2'
+
+    response_2_init = await communicator_2.receive_json_from(timeout=5)
+    assert response_2_init['type'] == 'notification'
+    assert len(response_2_init['new_messages']) == 0
+    response_2 = await communicator_2.receive_json_from(timeout=5)
+    assert response_2['type'] == 'message'
+    assert response_2['message'] == 'hi, user2'
+
+    await communicator_1.disconnect()
+    await communicator_2.disconnect()
+
+
+@pytest.mark.django_db(reset_sequences=True)
+@pytest.mark.asyncio
+async def test_websocket_with_token_send_message_bad_recipient_failure(
+        ws_url: str,
+        application: JWTAuthMiddlewareStack,
+        first_user: User,
+):
+    token_1 = AccessToken.for_user(first_user)
+    communicator_1 = WebsocketCommunicator(
+        application, f'{ws_url}?token={token_1}'
+    )
+    connected, subprotocol = await communicator_1.connect()
+    assert connected
+    await communicator_1.receive_json_from(timeout=5)  # init message
+
+    await communicator_1.send_json_to(
+        data={'message': 'hello!',
+              'sender': first_user.username,
+              'recipient': 'someone_idk'}
+    )
+    response_1 = await communicator_1.receive_json_from(timeout=5)
+    assert response_1['type'] == 'error'
+
+    await communicator_1.disconnect()
+
+
+@pytest.mark.django_db(reset_sequences=True)
+@pytest.mark.asyncio
+async def test_websocket_with_token_send_message_to_blocked_user_failure(
+        ws_url: str,
+        application: JWTAuthMiddlewareStack,
+        first_user: User,
+        second_user: User
+):
+    await sync_to_async(Block.objects.create)(initiator_user=first_user,
+                                        blocked_user=second_user)
+    token_1 = AccessToken.for_user(first_user)
+    communicator_1 = WebsocketCommunicator(
+        application, f'{ws_url}?token={token_1}'
+    )
+    connected, subprotocol = await communicator_1.connect()
+    assert connected
+    await communicator_1.receive_json_from(timeout=5)  # init message
+
+    await communicator_1.send_json_to(
+        data={'message': 'hello!',
+              'sender': first_user.username,
+              'recipient': second_user.username}
+    )
+
+    response_1 = await communicator_1.receive_json_from(timeout=5)
+    assert response_1['type'] == 'error'
+    assert response_1['message'] == 'You cannot message this user due to a block.'
+
+    token_2 = AccessToken.for_user(second_user)
+    communicator_2 = WebsocketCommunicator(
+        application, f'{ws_url}?token={token_2}'
+    )
+    connected, subprotocol = await communicator_2.connect()
+    assert connected
+    response_2_init = await communicator_2.receive_json_from(timeout=5)  # init message
+    assert response_2_init['type'] == 'notification'
+    # assert that blocked message was not sent
+    assert len(response_2_init['new_messages']) == 0
+
+    await communicator_1.disconnect()
+    await communicator_2.disconnect()
